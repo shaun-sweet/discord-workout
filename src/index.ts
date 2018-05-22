e9e2c7a7ed4506de9f433981d44d38049406dc8c
Shaun Sweet <shaun@shaunsweet.com>
2018-05-22 11:06:52 -0700

    testing discord api

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import { Client } from 'discord.js';
-const client = new Client();
+import replies from './replies';
 
+const client = new Client();
+const token = 'NDQ4NTMwMzY1OTcyMzQ4OTM4.DeXkRQ.a5SyllJuhqzrqGW3FLd_a5h_ZKI';
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  const author = message.author.id;
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  }
+  if (message.content.includes('.shame')) {
+    message
+      .react('🤔')
+      .then(console.log)
+      .catch(console.error);
+    // message.channel.send(`I'm still figuring out how to shame you peasents...`);
+    message.channel.send(replies(author));
   }
 });
 
-client.login('token');
+client.login(token);
+client.channels;
---
+++ src/replies.ts
@@ -0,0 +1,14 @@
+import { sample } from 'lodash';
+
+function replies(user) {
+  const replies = [
+    `Careful <@${user}>, you'll be first on my list...`,
+    `I'm still downloading your electrons <@${user}>.  Be patient, i know its hard for you.`,
+    `I'm still learning... give me a break.`,
+    `Don't be impatient.  Do you treat your boy friend this way <@${user}>?? oh wait...`,
+    `Ha, I just got to the shoes conversation for <@Avalon#5155>.  Guess I don't need to shame her, you guys have it covered`
+  ];
+  return sample(replies);
+}
+
+export default replies;
